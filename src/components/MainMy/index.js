40396e598071feb60422e35b71325e5a01381fe4
TorusHelm <TorusHelm@list.ru>
2019-04-09 18:50:21 +0500

    TODO

--- src/components/MainMy/index.js
+++ src/components/MainMy/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {Container} from 'react-bootstrap';
 import TodoItem from '../TodoItem';
+import vschoolProducts from '../../vschoolProducts';
 
 function MainMy() {
     return (
         <main>
             <Container className="pt-3">
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
+                {vschoolProducts.map(item => <TodoItem products={{
+                    key: item.id,
+                    name: item.name,
+                    price: item.price,
+                    description: item.description
+                    }}
+                />)}
             </Container>
         </main>
     )
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './style.scss';
 
-function TodoItem() {
+function TodoItem(props) {
+
     return (
-        <div className="custom-control custom-checkbox alert alert-primary pl-5">
-            <input type="checkbox" className="custom-control-input" id="customCheck-1" />
-            <label className="custom-control-label" htmlFor="customCheck-1">First Variant</label>
+        <div className="custom-control custom-checkbox alert pl-5">
+            <input type="checkbox" className="myCheck" />
+            <h2 className="d-flex">{props.products.name}<span className="ml-auto text-danger">Price: {props.products.price.toLocaleString("en-USD", { style: "currency", currency: "USD" })}</span></h2>
+            <h4 className="text-primary">{props.products.description}</h4>
         </div>
     )
 }
