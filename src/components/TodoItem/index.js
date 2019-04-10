b6780152158d7a1c0879eff4423e7fb8eaae46c9
TorusHelm <TorusHelm@list.ru>
2019-04-10 16:02:00 +0500

    save props into state component TodoItem

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import './style.scss';
 
-function TodoItem(props) {
+class TodoItem extends React.Component {
 
-    return (
-        <div className="custom-control custom-checkbox alert pl-5">
-            <input type="checkbox" className="myCheck" />
-            <h2 className="d-flex">{props.products.name}<span className="ml-auto text-danger">Price: {props.products.price.toLocaleString("en-USD", { style: "currency", currency: "USD" })}</span></h2>
-            <h4 className="text-primary">{props.products.description}</h4>
-        </div>
-    )
+    constructor(props) {
+        super(props)
+        const products = this.props.products;
+        this.state = {
+            name: products.name,
+            description: products.description,
+            price: products.price
+        }
+    }
+
+    changeHandler = () => {
+        console.log(this.state.name)
+    }
+    
+    render() {
+        return (
+            <div className="custom-control custom-checkbox alert pl-5">
+                <input onChange={this.changeHandler} type="checkbox" className="myCheck" />
+                <h2 className="d-flex">{this.props.products.name}<span className="ml-auto text-danger">Price: {this.props.products.price.toLocaleString("en-USD", { style: "currency", currency: "USD" })}</span></h2>
+                <h4 className="text-primary">{this.props.products.description}</h4>
+            </div>
+        )
+    }
 }
 
 export default TodoItem;
